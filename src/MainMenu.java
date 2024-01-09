3c6a6bf7abffc1c8b4172f94decd76cde2e4784a
Kale-Wu1 <kwu3@ocdsb.ca>
2024-01-08 23:36:14 -0500

    Test

---
+++ src/MainMenu.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class MainMenu extends JFrame{
+    public static void main(String[] args) {
+        new MainMenu();
+    }
+
+    public MainMenu() {
+        setBounds(0, 0, 600, 600);
+        JPanel mainFrame = new JPanel();
+        setVisible(true);
+
+
+
+
+    }
+}
